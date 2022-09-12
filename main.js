741fef1a5e68fb73b816d0444e6fec992784bd50
minagrullon <minagrullon@pursuit.org>
2022-09-12 08:04:44 -0400

    some html

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+const input = document.querySelector(".poetName");
+const container = document.querySelector(".container");
+const ul = document.querySelector("ul");
+
 const URL = "https://poetrydb.org/author";
 
 fetch(URL)
   .then((data) => data.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+  .then((data) => {
+    for (let i = 0; i < data.authors.length; i++) {
+      console.log(data.authors);
+      let li = document.createElement("li");
+      li.textContent = data.authors[i];
+      console.log(li);
+      ul.append(li);
+    }
+  })
+  .catch((err) => console.log("ERROR"));
