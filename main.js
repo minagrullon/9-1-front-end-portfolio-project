2e7343e8c1b4fb05c7ad9781b282eaef7b13c18d
minagrullon <minagrullon@pursuit.org>
2022-09-13 15:25:12 -0400

    form function

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const input = document.querySelector(".poetName");
 const form = document.querySelector("form");
 const container = document.querySelector(".container");
 const divMain = document.querySelector("main div");
+const h4Main = document.querySelector("main h4");
 
 const URL = "https://poetrydb.org/author";
 
@@ -19,4 +20,26 @@ fetch(URL)
   })
   .catch((err) => console.log(err));
 
-form.addEventListener("submit", (e) => {});
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let search = input.value;
+  console.log(search);
+  fetch(`${URL}/${search}`)
+    .then((data) => data.json())
+    .then((data) => {
+      h4Main.innerHTML = "";
+      h4Main.innerHTML = `<h2>${search}</h2>`;
+      divMain.innerHTML = "";
+      const poemHolder = document.createElement("div");
+      divMain.prepend(poemHolder);
+      data.forEach((poem) => {
+        const title = document.createElement("h3");
+        title.innerHTML = `<u>Title:</u> ${poem.title}`;
+        console.log(title);
+        const poemP = document.createElement("p");
+        poemP.textContent = poem.lines;
+        poemHolder.append(title, poemP);
+      });
+    })
+    .catch((err) => console.log(err));
+});
