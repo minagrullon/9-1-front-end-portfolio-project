7ee3dd77b830213d14660ddcd40ebd73aa2b0f2a
minagrullon <minagrullon@pursuit.org>
2022-09-15 14:35:59 -0400

    fixed input and format

--- main.js
+++ main.js
@@ -24,20 +24,29 @@ fetch(URL)
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let search = input.value;
-  console.log(search);
+
   fetch(`${URL}/${search}`)
     .then((data) => data.json())
     .then((data) => {
-      console.log(data);
       if (data.status && data.status === 404) {
         error.style.display = "block";
         error.style.color = "red";
-        error.style.fontWeight = "700";
         return;
       }
 
+      error.style.display = "none";
+
+      const inputArray = search.split(" ");
+      let poetTitle = "";
+      for (let word of inputArray) {
+        word = word[0].toUpperCase() + word.slice(1).toLowerCase();
+        console.log(word);
+        poetTitle += " " + word;
+      }
+
       h3Main.innerHTML = "";
-      h3Main.innerHTML = `<h2>${search}</h2>`;
+      h3Main.style.textDecoration = "underline";
+      h3Main.innerHTML = `<h3><em>${poetTitle}</em></h3>`;
       divMain.innerHTML = "";
       const poemHolder = document.createElement("div");
       divMain.prepend(poemHolder);
@@ -46,7 +55,15 @@ form.addEventListener("submit", (e) => {
         title.innerHTML = `<u>Title:</u> ${poem.title}`;
 
         const poemP = document.createElement("p");
-        poemP.textContent = poem.lines;
+        let poemContent = poem.lines;
+        let content = "";
+        poemContent.forEach((line) => {
+          content += line + "\n";
+        });
+
+        console.log(content);
+
+        poemP.innerText = content;
         poemHolder.append(title, poemP);
       });
     })
